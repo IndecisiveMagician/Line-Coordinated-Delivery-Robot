433b0b33a691ff3078a0dfda2f417f8abf9dd170
jt1938 <112014839+jt1938@users.noreply.github.com>
2023-03-21 09:08:27 -0400

    changed pin for motor B power
    changed from 18 to 28

--- robotCode/movementCodePrototype.py
+++ robotCode/movementCodePrototype.py
@@ -14,7 +14,7 @@ GPIO.setup(12, GPIO.OUT) # Connected to AIN1
 GPIO.setup(13, GPIO.OUT) # Connected to STBY
 GPIO.setup(15, GPIO.OUT) # Connected to BIN1
 GPIO.setup(16, GPIO.OUT) # Connected to BIN2
-GPIO.setup(18, GPIO.OUT) # Connected to PWMB
+GPIO.setup(28, GPIO.OUT) # Connected to PWMB
 
 # Wait 60 seconds
 time.sleep(60)
@@ -31,7 +31,7 @@ GPIO.output(16, GPIO.LOW) # Set BIN2
 # Motor A:
 GPIO.output(7, GPIO.HIGH) # Set PWMA
 # Motor B:
-GPIO.output(18, GPIO.HIGH) # Set PWMB
+GPIO.output(28, GPIO.HIGH) # Set PWMB
 
 # Disable STBY (standby)
 GPIO.output(13, GPIO.HIGH)
@@ -51,7 +51,7 @@ GPIO.output(16, GPIO.HIGH) # Set BIN2
 # Motor A:
 GPIO.output(7, GPIO.HIGH) # Set PWMA
 # Motor B:
-GPIO.output(18, GPIO.HIGH) # Set PWMB
+GPIO.output(28, GPIO.HIGH) # Set PWMB
 
 # Disable STBY (standby)
 GPIO.output(13, GPIO.HIGH)
@@ -66,4 +66,4 @@ GPIO.output(7, GPIO.LOW) # Set PWMA
 GPIO.output(13, GPIO.LOW) # Set STBY
 GPIO.output(15, GPIO.LOW) # Set BIN1
 GPIO.output(16, GPIO.LOW) # Set BIN2
-GPIO.output(18, GPIO.LOW) # Set PWMB
+GPIO.output(28, GPIO.LOW) # Set PWMB
